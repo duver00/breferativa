6b52a5d342929a7ac6ee84b4b5d2af4a51d6f7fe
Duver <duver089@gmail.com>
2021-09-02 12:54:42 -0400

    cambios de despliegue1

--- Scripts/django-admin.py
+++ Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!D:\Dev\Proyectos\Django\breferativa\Scripts\python.exe
+#!D:\DEV\Proyectos\Django\breferativa\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
--- breferativa/settings.py
+++ breferativa/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -28,14 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
@@ -74,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'breferativa.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -83,12 +80,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'referativa',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'libercuba',
         'HOST': 'localhost',
-        'DATABASE_PORT': '5433',
+        'DATABASE_PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -108,11 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -122,9 +117,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 INTERNAL_IPS = ['127.0.0.1']
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
